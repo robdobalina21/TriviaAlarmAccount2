8271c17555922504b6b500c72c2567973495f132
robdobalina21 <robdobalina21@gmail.com>
2026-07-29 19:54:12 -0400

    Delete netlify/functions directory

--- netlify/functions/logout.mjs
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie, json } from './lib/session.mjs';
-
-export default async (req) => {
-  const isHttps = new URL(req.url).protocol === 'https:';
-  return json({ ok: true }, 200, { 'Set-Cookie': clearSessionCookie(isHttps) });
-};
